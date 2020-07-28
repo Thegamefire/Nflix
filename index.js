d8a901e42f8b87b24ae13f85a913718909d7642a
Thegamefire <60431842+Thegamefire@users.noreply.github.com>
2020-07-28 23:06:30 +0200

    First row is now invisible
    second row is configured for movie 1

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let conditionF = false
 function openNav() {
+	alert("hoj")
    document.getElementById("mySidenav").style.display = "block";
-   document.getElementById("box1").style.top = "400px";
-   document.getElementById("box1").style.border = "1px solid #fff";
-   document.getElementById("box1").style.transform = "scale(1)";
-   document.getElementById("row1").style.height = "800px";
+   document.getElementById("box2.1").style.top = "400px";
+   document.getElementById("box2.1").style.border = "1px solid #fff";
+   document.getElementById("box2.1").style.transform = "scale(1)";
+   document.getElementById("row2").style.height = "800px";
 
    // document.getElementById("box1").style.display = "block";  
 }
