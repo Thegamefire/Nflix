91cca629da64eff2e938155c2275676cc2ee9b14
Thegamefire <60431842+Thegamefire@users.noreply.github.com>
2020-07-28 22:51:48 +0200

    Sidebar nav open and close bug fixed

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-function openNav(x, y, z) {
-   document.getElementById(x).style.display = "block";
-   document.getElementById(y).style.top = "400px";
-   document.getElementById(y).style.border = "1px solid #fff";
-   document.getElementById(y).style.transform = "scale(1)";
-   document.getElementById(z).style.height = "800px";
+let conditionF = false
+function openNav() {
+   document.getElementById("mySidenav").style.display = "block";
+   document.getElementById("box1").style.top = "400px";
+   document.getElementById("box1").style.border = "1px solid #fff";
+   document.getElementById("box1").style.transform = "scale(1)";
+   document.getElementById("row1").style.height = "800px";
 
-   // document.getElementById("box1").style.display = "block";
+   // document.getElementById("box1").style.display = "block";  
 }
 
 function closeNav() {
@@ -13,4 +14,10 @@ function closeNav() {
    document.getElementById("box1").style.transform = "scale(1)";
    document.getElementById("box1").style.transition = "all 0.7s ease";
    document.getElementById("box1").style.border = "none";
+   document.getElementById("row1").style.height = "250px";
+
+}
+if (conditionF) {
+	openNav
+	closeNav
 }
