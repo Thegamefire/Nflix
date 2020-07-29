04024e4a9e399acdab2340b5fa20e6fca56362bc
Thegamefire <60431842+Thegamefire@users.noreply.github.com>
2020-07-29 22:20:15 +0200

    All movies work

---
+++ nav.js
@@ -0,0 +1,196 @@
+let conditionF = false
+function openNav1_1() {
+   document.getElementById("mySidenav1.1").style.display = "block";
+   document.getElementById("box1.1").style.top = "400px";
+   document.getElementById("box1.1").style.border = "1px solid #fff";
+   document.getElementById("box1.1").style.transform = "scale(1)";
+   document.getElementById("row1").style.height = "800px";
+
+   // document.getElementById("box1").style.display = "block";  
+}
+function closeNav1_1() {
+   document.getElementById("mySidenav1.1").style.display = "none";
+   document.getElementById("box1.1").style.transform = "scale(1)";
+   document.getElementById("box1.1").style.transition = "all 0.7s ease";
+   document.getElementById("box1.1").style.border = "none";
+   document.getElementById("row1").style.height = "250px";
+
+
+}
+function openNav1_2() {
+   document.getElementById("mySidenav1.2").style.display = "block";
+   document.getElementById("box1.2").style.top = "400px";
+   document.getElementById("box1.2").style.border = "1px solid #fff";
+   document.getElementById("box1.2").style.transform = "scale(1)";
+   document.getElementById("row1").style.height = "800px";
+
+   // document.getElementById("box1").style.display = "block";  
+}
+function closeNav1_2() {
+   document.getElementById("mySidenav1.2").style.display = "none";
+   document.getElementById("box1.2").style.transform = "scale(1)";
+   document.getElementById("box1.2").style.transition = "all 0.7s ease";
+   document.getElementById("box1.2").style.border = "none";
+   document.getElementById("row1").style.height = "250px";
+
+
+}
+function openNav1_3() {
+   document.getElementById("mySidenav1.3").style.display = "block";
+   document.getElementById("box1.3").style.top = "400px";
+   document.getElementById("box1.3").style.border = "1px solid #fff";
+   document.getElementById("box1.3").style.transform = "scale(1)";
+   document.getElementById("row1").style.height = "800px";
+
+   // document.getElementById("box1").style.display = "block";  
+}
+function closeNav1_3() {
+   document.getElementById("mySidenav1.1").style.display = "none";
+   document.getElementById("box1.1").style.transform = "scale(1)";
+   document.getElementById("box1.1").style.transition = "all 0.7s ease";
+   document.getElementById("box1.1").style.border = "none";
+   document.getElementById("row1").style.height = "250px";
+
+
+}
+
+function openNav2_1() {
+   document.getElementById("mySidenav2.1").style.display = "block";
+   document.getElementById("box2.1").style.top = "400px";
+   document.getElementById("box2.1").style.border = "1px solid #fff";
+   document.getElementById("box2.1").style.transform = "scale(1)";
+   document.getElementById("row2").style.height = "800px";
+
+   // document.getElementById("box1").style.display = "block";  
+}
+
+function closeNav2_1() {
+   document.getElementById("mySidenav2.1").style.display = "none";
+   document.getElementById("box2.1").style.transform = "scale(1)";
+   document.getElementById("box2.1").style.transition = "all 0.7s ease";
+   document.getElementById("box2.1").style.border = "none";
+   document.getElementById("row2").style.height = "250px";
+}
+	
+	function openNav2_2() {
+   document.getElementById("mySidenav2.2").style.display = "block";
+   document.getElementById("box2.2").style.top = "400px";
+   document.getElementById("box2.2").style.border = "1px solid #fff";
+   document.getElementById("box2.2").style.transform = "scale(1)";
+   document.getElementById("row2").style.height = "800px";
+
+   // document.getElementById("box1").style.display = "block";  
+}
+
+function closeNav2_2() {
+   document.getElementById("mySidenav2.2").style.display = "none";
+   document.getElementById("box2.2").style.transform = "scale(1)";
+   document.getElementById("box2.2").style.transition = "all 0.7s ease";
+   document.getElementById("box2.2").style.border = "none";
+   document.getElementById("row2").style.height = "250px";
+
+
+}
+	
+	function openNav2_3() {
+   document.getElementById("mySidenav2.3").style.display = "block";
+   document.getElementById("box2.3").style.top = "400px";
+   document.getElementById("box2.3").style.border = "1px solid #fff";
+   document.getElementById("box2.3").style.transform = "scale(1)";
+   document.getElementById("row2").style.height = "800px";
+
+   // document.getElementById("box1").style.display = "block";  
+}
+
+function closeNav2_3() {
+   document.getElementById("mySidenav2.3").style.display = "none";
+   document.getElementById("box2.3").style.transform = "scale(1)";
+   document.getElementById("box2.3").style.transition = "all 0.7s ease";
+   document.getElementById("box2.3").style.border = "none";
+   document.getElementById("row2").style.height = "250px";
+
+
+}
+	
+	function openNav3_1() {
+   document.getElementById("mySidenav3.1").style.display = "block";
+   document.getElementById("box3.1").style.top = "400px";
+   document.getElementById("box3.1").style.border = "1px solid #fff";
+   document.getElementById("box3.1").style.transform = "scale(1)";
+   document.getElementById("row3").style.height = "800px";
+
+   // document.getElementById("box1").style.display = "block";  
+}
+
+function closeNav3_1() {
+   document.getElementById("mySidenav3.1").style.display = "none";
+   document.getElementById("box3.1").style.transform = "scale(1)";
+   document.getElementById("box3.1").style.transition = "all 0.7s ease";
+   document.getElementById("box3.1").style.border = "none";
+   document.getElementById("row3").style.height = "250px";
+
+
+}
+	
+	function openNav3_2() {
+   document.getElementById("mySidenav3.2").style.display = "block";
+   document.getElementById("box3.2").style.top = "400px";
+   document.getElementById("box3.2").style.border = "1px solid #fff";
+   document.getElementById("box3.2").style.transform = "scale(1)";
+   document.getElementById("row3").style.height = "800px";
+
+   // document.getElementById("box1").style.display = "block";  
+}
+
+function closeNav3_2() {
+   document.getElementById("mySidenav3.2").style.display = "none";
+   document.getElementById("box3.2").style.transform = "scale(1)";
+   document.getElementById("box3.2").style.transition = "all 0.7s ease";
+   document.getElementById("box3.2").style.border = "none";
+   document.getElementById("row3").style.height = "250px";
+
+
+}
+	
+	function openNav3_3() {
+   document.getElementById("mySidenav3.3").style.display = "block";
+   document.getElementById("box3.3").style.top = "400px";
+   document.getElementById("box3.3").style.border = "1px solid #fff";
+   document.getElementById("box3.3").style.transform = "scale(1)";
+   document.getElementById("row3").style.height = "800px";
+
+   // document.getElementById("box1").style.display = "block";  
+}
+
+function closeNav3_3() {
+   document.getElementById("mySidenav3.3").style.display = "none";
+   document.getElementById("box3.3").style.transform = "scale(1)";
+   document.getElementById("box3.3").style.transition = "all 0.7s ease";
+   document.getElementById("box3.3").style.border = "none";
+   document.getElementById("row3").style.height = "250px";
+
+
+}
+	
+if (conditionF) {
+	openNav1_1
+	closeNav1_1
+	openNav1_2
+	closeNav1_2
+	openNav1_3
+	closeNav1_3
+	
+	openNav2_1
+	closeNav2_1
+	openNav2_2
+	closeNav2_2
+	openNav2_3
+	closeNav2_3
+	
+	openNav3_1
+	closeNav3_1
+	openNav3_2
+	closeNav3_2
+	openNav3_3
+	closeNav3_3
+}
